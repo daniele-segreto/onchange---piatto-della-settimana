5dc414517adcc8408f3c175176fa703a092821c1
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-09-15 22:57:36 +0200

    first commit

---
+++ js/dati.js
@@ -0,0 +1,55 @@
+let lunedi = {
+    img: 'https://cdn.robadadonne.it/wp-content/uploads/sites/4/2018/07/12172347/ricetta-lasagne-ricotta-spinaci20180712032347_7869f9b8fb34cd74e642a14485369d3220180712032347_d46874c91d99be0dc6383aed924fc6db.jpg',
+    piatto: 'Lasagne al forno con ricotta e spinaci',
+    ingredienti: 'Pasta per lasagne, Spinaci freschi, Ricotta, Parmigiano grattugiato, Mozzarella, Salsa di pomodoro, Burro, Farina, Latte, Noce moscata, Sale e pepe.',
+    footer: 'Il piatto del Lunedì',
+    alt: 'lasagne'
+}
+
+let martedi = {
+    img: 'https://blog.giallozafferano.it/adryincucina/wp-content/uploads/2019/11/risotto-ai-funghi-porcini.jpg',
+    piatto: 'Risotto ai funghi porcini con parmigiano e prezzemolo',
+    ingredienti: "Riso Arborio, Funghi porcini freschi o secchi (ammollati), Brodo vegetale, Cipolla, Burro, Parmigiano grattugiato, Prezzemolo fresco, Olio d'oliva, Sale e pepe",
+    footer: 'Il piatto del Martedì',
+    alt: 'riotto'
+}
+
+let mercoledi = {
+    img: 'https://pasticcerialievito.it/wp-content/uploads/pollo-in-padella-al-limone-e-rosmarino.jpg',
+    piatto: 'Pollo al limone con patate al rosmarino',
+    ingredienti: "Petto di pollo, Limoni, Patate, Rosmarino fresco, Aglio, Olio d'oliva, Sale e pepe",
+    footer: 'Il piatto del Mercoledì',
+    alt: 'pollo'
+}
+
+let giovedi = {
+    img: 'https://i2.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2019/07/4007_Spaghetti.jpg?resize=895%2C616&ssl=1',
+    piatto: 'Spaghetti aglio, olio e peperoncino con pomodorini freschi e basilico',
+    ingredienti: "Spaghetti, Aglio, Peperoncino fresco o in fiocchi, Pomodorini ciliegia, Olio d'oliva, Basilico fresco, Sale",
+    footer: 'Il piatto del Giovedì',
+    alt: 'spaghetti'
+}
+
+let venerdi = {
+    img: 'https://wips.plug.it/cips/buonissimo.org/cms/2019/11/filetto-di-salmone-con-salsa-di-miele-e-senape.jpg?w=712&a=c&h=406',
+    piatto: 'Salmone grigliato con salsa di senape e miele, accompagnato da riso basmati e asparagi',
+    ingredienti: "Filetto di salmone, Senape di Digione, Miele, Limone, Riso basmati, Asparagi, Olio d'oliva, Sale e pepe",
+    footer: 'Il piatto del Venerdì',
+    alt: 'salmone'
+}
+
+let sabato = {
+    img: 'https://wips.plug.it/cips/buonissimo.org/cms/2011/12/pizza-al-prosciutto-cotto.jpg?w=713&a=c&h=407',
+    piatto: 'Pizza fatta in casa con pomodoro, mozzarella, funghi e prosciutto cotto',
+    ingredienti: "Pasta per pizza (o puoi farla in casa con farina, lievito, acqua, sale e olio d'oliva), Pomodoro passato, Mozzarella, Funghi freschi, Prosciutto cotto, Olio d'oliva, Sale e pepe",
+    footer: 'Il piatto del Sabato',
+    alt: 'pizza'
+}
+
+let domenica = {
+    img: 'https://www.nonnabox.com/wp-content/uploads/regional-recipes-ossobuco-milanese.jpg',
+    piatto: 'Osso buco con risotto alla milanese',
+    ingredienti: 'Fette di vitello per osso buco, Farina, Burro, Cipolla, Sedano, Carota, Brodo di carne, Vino bianco, Zafferano, Riso, Parmigiano grattugiato, Burro',
+    footer: 'Il piatto della Domenica',
+    alt: 'osso buco'
+}
